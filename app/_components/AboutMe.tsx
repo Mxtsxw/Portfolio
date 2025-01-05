2365621073a9d91df2465a5084b7ce50c832b0fa
Matthieu <randriantsoamatthieu@gmail.com>
2025-01-05 18:54:28 +0100

    fix quote encoding

--- app/_components/AboutMe.tsx
+++ app/_components/AboutMe.tsx
@@ -21,13 +21,13 @@ export default function AboutMe() {
         <div className="mt-4 md:mt-0 md:ml-4 w-full md:w-1/2 text-gray-400">
           <h2 className="text-2xl font-bold">MORE ABOUT ME</h2>
           <p className="mt-2">
-          I’m a computer science engineer with a <span className="text-white font-bold">strong focus on artificial intelligence and data science</span>, currently advancing my expertise at Polytech Lyon and the University of Lyon. My work revolves around solving real-world challenges through <span className="text-white font-bold">machine learning and innovative algorithms</span>.
+          I'm a computer science engineer with a <span className="text-white font-bold">strong focus on artificial intelligence and data science</span>, currently advancing my expertise at Polytech Lyon and the University of Lyon. My work revolves around solving real-world challenges through <span className="text-white font-bold">machine learning and innovative algorithms</span>.
           </p>
           <p className="mt-2">
-          I’m deeply interested in exploring how AI can make a difference in security and healthcare. Whether it’s improving decision-making systems or tackling complex data problems, I’m drawn to projects where technology meets <span className="text-white font-bold">critical impact</span>.
+          I'm deeply interested in exploring how AI can make a difference in security and healthcare. Whether it's improving decision-making systems or tackling complex data problems, I'm drawn to projects where technology meets <span className="text-white font-bold">critical impact</span>.
           </p>
           <p className="mt-2">
-          Outside of tech, I’ve spent four years competing in <span className="text-white font-bold">volleyball</span>, developing resilience and teamwork. I also have a passion for <span className="text-white font-bold">filmmaking</span>. Both pursuits reflect my drive for growth and exploration across diverse fields.
+          Outside of tech, I've spent four years competing in <span className="text-white font-bold">volleyball</span>, developing resilience and teamwork. I also have a passion for <span className="text-white font-bold">filmmaking</span>. Both pursuits reflect my drive for growth and exploration across diverse fields.
           </p>
         </div>
       </div>
