0b95ae9f7725c16e76da74b2c57f70425488d130
Matthieu <randriantsoamatthieu@gmail.com>
2025-03-12 18:33:32 +0100

    Remove Floating buttons

--- app/_components/Header.tsx
+++ app/_components/Header.tsx
@@ -41,42 +41,6 @@ export default function Hero() {
           />
         </div>
       </div>
-      {/* Buttons for Small Screens */}
-      <div className="flex lg:hidden mt-8 space-x-4 justify-center tmp">
-        <a
-          href="documents/CV_Matthieu_RANDRIANTSOA.pdf"
-          target="_blank"
-          className="bg-sky-600 hover:bg-sky-700 text-white font-semibold py-3 px-6 rounded-lg shadow-md"
-        >
-          My Resume
-        </a>
-        <a
-          href="documents/CL_Matthieu_Randriantsoa_Roche.pdf"
-          target="_blank"
-          className="bg-gray-800 hover:bg-gray-700 text-white font-semibold py-3 px-6 rounded-lg shadow-md"
-        >
-          My Cover Letter
-        </a>
-      </div>
-
-      {/* Buttons for Large Screens */}
-      <div className="hidden lg:flex lg:justify-start mt-8 space-x-4 justify-center tmp">
-        <a
-          href="documents/CV_Matthieu_RANDRIANTSOA.pdf"
-          target="_blank"
-          className="bg-sky-600 hover:bg-sky-700 text-white font-semibold py-3 px-6 rounded-lg shadow-md transform transition-all duration-300 animate-float"
-        >
-          My Resume
-        </a>
-        <a
-          href="documents/CL_Matthieu_Randriantsoa_Roche.pdf"
-          target="_blank"
-          className="bg-gray-800 hover:bg-gray-700 text-white font-semibold py-3 px-6 rounded-lg shadow-md transform transition-all duration-300 animate-float delay-200"
-        >
-          My Cover Letter
-        </a>
-      </div>
-
     </header>
   );
 }
